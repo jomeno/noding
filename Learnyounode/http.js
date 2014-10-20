d9129e1a8ce35dbd86f99f95006d447bfb73265e
Jomeno <jomeno.vona@gmail.com>
2014-10-20 23:47:26 +0100

    Completed Lesson 7

--- Learnyounode/http.js
+++ Learnyounode/http.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var url = process.argv[2];
-console.log(url);
 
-var result = http.get(url);
-console.log(result);
+http.get(url, function(response){
+response.setEncoding('utf8');
+response.on("data", console.log);
+response.on("error", console.error);
+
+});
