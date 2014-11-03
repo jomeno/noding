5aa4b85f26ed1e9a32bc6dc614ce40e99415d611
Jomeno <jomeno.vona@gmail.com>
2014-11-03 11:37:06 +0100

    Spat data from node on web page

---
+++ Learnyounode/RadialCircle/public/app/controllers/monitor.js
@@ -0,0 +1,18 @@
+﻿app.controller("Monitor", function ($scope, $http) {
+    
+    var models = {
+        readings : ['just test']
+    }
+
+    // get readings from server
+
+    $http.get('/public/api').then(function (response) {
+        console.log('http call started')
+        models.readings[0] = response.data;
+    });
+
+
+    angular.extend($scope, {
+        readings : models.readings
+    })
+})
